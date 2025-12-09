64e0996ea2972b183a655bf35b6784a16463ae3f
Alethea Almas <alethea.almas@gmail.com>
2025-12-09 11:17:31 +0800

    feat: profile component

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,15 @@
+import {Profile} from "./profile"
+
 export function App() {
   return (
     <div>
       <h1>PeachPlan di halaman app tsx</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+      <Profile />
+      <Profile />
     </div>
   );
 }
+
