865f89b4db111d26bbc821be88eea453fa81d9d9
Alethea Almas <alethea.almas@gmail.com>
2025-12-09 13:09:04 +0800

    feat: looping array project list

--- src/projects.tsx
+++ src/projects.tsx
@@ -1,14 +1,20 @@
+const dataProjects = [
+  { id: 1, title: "Personal Web", isDone: true },
+  { id: 2, title: "CRM", isDone: true },
+  { id: 3, title: "Task Management", isDone: false },
+  { id: 4, title: "Information API", isDone: false },
+  { id: 5, title: "Simple Ecommerce", isDone: false },
+  { id: 6, title: "Personal Projects", isDone: false },
+];
+
 export function Projects() {
   return (
     <div>
       <h2>Projects</h2>
       <ul>
-        <ProjectItem title="Personal Web" isDone/>
-        <ProjectItem title="CRM" isDone/>
-        <ProjectItem title="Task Management" isDone={false} />
-        <ProjectItem title="Information API" isDone={false} />
-        <ProjectItem title="Simple Ecommerce" isDone={false} />
-        <ProjectItem title="Personal Project" isDone={false} />
+        {dataProjects.map((project) => (
+          <ProjectItem title={project.title} isDone={project.isDone} />
+        ))}
       </ul>
     </div>
   );
