393e174862b0ec2de8969181b7f381e506f3b74a
Alethea Almas <alethea.almas@gmail.com>
2025-12-11 14:54:13 +0800

    feat: add welcome section

--- src/app.tsx
+++ src/app.tsx
@@ -1,34 +1,45 @@
 import { SearchBar } from "./components/search-bar";
 import { Button } from "./components/button";
+import { WelcomeSection } from "./components/welcome-section";
+import { Projects } from "./components/projects";
 
 export function App() {
   return (
-    <div>
+    <div className="bg-stone-50">
       <header>
-        <div className="w-full flex justify-between px-3 py-2 border-b border-gray-200 bg-red-200">
-          <img
-            className="flex gap-3"
-            src="/logo.svg"
-            alt="Peach Plan Logo"
-            width={50}
-            height={30}
-          />
+        <div className="w-full border-b border-gray-200 bg-red-200">
+          <div className="max-w-4xl mx-auto flex justify-between items-center px-3 py-2">
+            <img
+              className="flex gap-3"
+              src="/logo.svg"
+              alt="Peach Plan Logo"
+              width={50}
+              height={30}
+            />
 
-          <SearchBar />
-          <div className="flex justify-between gap-4">
-            <Button label="+ Add Project" />
-            <span className="w-10 h-10 rounded-full bg-gray-200">
-              <img
-                className="flex p-1"
-                src="/avatar.svg"
-                alt="User Avatar"
-                width={50}
-                height={30}
-              />
-            </span>
+            <SearchBar />
+            <div className="flex justify-between gap-4">
+              <Button label="+ Add Project" />
+              <span className="w-10 h-10 rounded-full overflow-hidden bg-gray-200">
+                <img
+                  className="p-1 object-cover"
+                  src="/avatar.svg"
+                  alt="User Avatar"
+                  width={50}
+                  height={30}
+                />
+              </span>
+            </div>
           </div>
         </div>
       </header>
+      <main className="p-4 max-w-4xl mx-auto">
+        <div>
+        <WelcomeSection />
+        </div>
+
+        <Projects />
+      </main>
     </div>
   );
 }
--- src/components/welcome-section.tsx
+++ src/components/welcome-section.tsx
@@ -12,15 +12,19 @@ export function WelcomeSection() {
   });
 
   return (
-    <div>
-      <div>
-        <h2>Welcome</h2>
-        <p>Lets get things done, peachfully</p>
-      </div>
+    <div className="border-2 border-gray-400 rounded-lg p-4 mt-6">
+      <div className="flex justify-between items-start">
+        
+        <div>
+          <h2 className="text-2xl font-semibold">Welcome,</h2>
+          <p className="text-gray-600 mt-1">Lets get things done, peachfully!</p>
+        </div>
 
-      <div>
-        <h2>{month}</h2>
-        <p>Today is {fullDate}</p>
+    
+        <div className="text-right">
+          <h2 className="text-xl font-semibold">{month}</h2>
+          <p className="text-gray-600">Today is {fullDate}</p>
+        </div>
       </div>
     </div>
   );
