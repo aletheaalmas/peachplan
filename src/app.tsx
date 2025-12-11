ff24b95727cce8519ab388c5e82f1af3ab0e964a
Alethea Almas <alethea.almas@gmail.com>
2025-12-11 12:58:34 +0800

    feat: move logo from header to app

--- src/app.tsx
+++ src/app.tsx
@@ -5,6 +5,9 @@ import { Projects } from "./projects";
 export function App() {
   return (
     <div>
+      <div>
+        <img src="/logo.svg" alt="Peach Plan Logo" />
+      </div>
       <Header />
       <WelcomeSection />
       <Projects />
--- src/header.tsx
+++ src/header.tsx
@@ -1,21 +1,8 @@
-import logoImage from "./assets/logo.svg";
 export function Header() {
   return (
     <header>
       <div>
-        <img src={logoImage} alt="Peach Plan Logo" />
-      </div>
-
-      <div>
-        <input
-          type="text"
-          placeholder="Search your projects"
-          className="
-           max-w-md px-4 py-2
-            border border-gray-300 rounded-lg
-            placeholder:text-gray-400 text-gray-700
-          "
-        />
+        <input type="text" placeholder="Search your projects" />
       </div>
 
       <button>+ Add Project</button>
