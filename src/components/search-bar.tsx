c0f2909f910406e1ffff4d1153ea3a116f69a780
Alethea Almas <alethea.almas@gmail.com>
2025-12-11 13:54:34 +0800

    chore: move components into folder

--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -2,7 +2,7 @@ export function SearchBar() {
   return (
     <div className="relative w-full max-w-md">
       <span className="absolute left-3 top-1/2 -translate-y-1/2">
-        <img src="/icons/search.svg" alt="Search Icon" width="15" height="15" />
+        <img src="/icons/search.svg" alt="Search Icon" width="15" height="16" />
       </span>
       <input
         type="text"
@@ -11,7 +11,7 @@ export function SearchBar() {
           pl-10 pr-4 py-2
           border-2 border-gray-300 rounded-lg
           placeholder:text-gray-400 text-gray-700
-          focus:outline-none focus:ring-2 focus:ring-red-400
+          focus:outline-none focus:ring-2 focus:ring-red-300
           "
       />
     </div>
