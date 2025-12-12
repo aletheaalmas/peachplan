9b9f91592822de53a5d393e424b16bd9a24cfd20
M Haidar Hanif <git@mhaidarhanif.com>
2025-12-12 09:06:04 +0700

    fix: search bar structure with form and input props

--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -1,14 +1,15 @@
 export function SearchBar() {
   return (
-    <div className="relative w-full max-w-md">
+    <form className="relative w-full max-w-md">
       <span className="absolute top-1/2 left-3 -translate-y-1/2">
-        <img src="/icons/search.svg" alt="Search Icon" width="15" height="16" />
+        <img src="/icons/search.svg" alt="Search Icon" width="15" height="15" />
       </span>
       <input
-        type="text"
+        name="q"
+        type="search"
         placeholder="Search your projects"
         className="w-full rounded-lg border-2 border-gray-300 bg-white py-2 pr-4 pl-10 text-gray-700 placeholder:text-gray-400 focus:ring-2 focus:ring-red-300 focus:outline-none"
       />
-    </div>
+    </form>
   );
 }
