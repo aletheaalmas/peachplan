1d2a4a0b745f3a8fcd27074cfea6eee74c0714be
M Haidar Hanif <git@mhaidarhanif.com>
2025-12-12 09:35:20 +0700

    refactor: move project data and types to module project

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,50 +1,5 @@
-export type Project = {
-  id: number;
-  title: string;
-  isDone: boolean;
-  priority: "p1" | "p2" | "p3" | "p4";
-  dueDate: Date;
-};
-
-export type Projects = Project[];
-
-const dataProjects: Projects = [
-  {
-    id: 1,
-    title: "Personal Web",
-    isDone: true,
-    priority: "p1",
-    dueDate: new Date("2025-10-01"),
-  },
-  {
-    id: 2,
-    title: "Contact Management",
-    isDone: true,
-    priority: "p2",
-    dueDate: new Date("2025-11-01"),
-  },
-  {
-    id: 3,
-    title: "Task Management",
-    isDone: false,
-    priority: "p2",
-    dueDate: new Date("2025-12-01"),
-  },
-  {
-    id: 4,
-    title: "Information API",
-    isDone: false,
-    priority: "p3",
-    dueDate: new Date("2026-01-01"),
-  },
-  {
-    id: 5,
-    title: "Simple Ecommerce",
-    isDone: false,
-    priority: "p4",
-    dueDate: new Date("2026-02-01"),
-  },
-];
+import { dataProjects } from "../modules/project/data";
+import type { Project } from "../modules/project/type";
 
 export function Projects() {
   return (
