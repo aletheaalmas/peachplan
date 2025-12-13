a8ca94f215d1b0c56e05f217376a0d43b4105ab8
Alethea Almas <alethea.almas@gmail.com>
2025-12-13 23:31:17 +0800

    refactor: app component structure

--- src/app.tsx
+++ src/app.tsx
@@ -5,10 +5,10 @@ import { Projects } from "./components/projects";
 
 export function App() {
   return (
-    <div >
-      <header>
-        <div className="w-full border-b border-gray-200 bg-red-200">
-          <div className="mx-auto flex max-w-4xl items-center justify-between px-3 py-2">
+    <>
+      <header className="w-full border-b border-gray-200 bg-red-200">
+        <nav className="mx-auto flex max-w-4xl items-center justify-between px-3 py-2">
+          <a href="/">
             <img
               className="flex gap-3"
               src="/logo.svg"
@@ -16,22 +16,23 @@ export function App() {
               width={50}
               height={30}
             />
+          </a>
 
-            <SearchBar />
-            <div className="flex justify-between gap-4">
-              <Button label="+ Add Project" />
-              <span className="h-10 w-10 overflow-hidden rounded-full bg-gray-200">
-                <img
-                  className="object-cover p-1"
-                  src="/avatar.svg"
-                  alt="User Avatar"
-                  width={50}
-                  height={30}
-                />
-              </span>
-            </div>
+          <SearchBar />
+
+          <div className="flex justify-between gap-4">
+            <Button label="+ Add Project" />
+            <span className="h-10 w-10 overflow-hidden rounded-full bg-gray-200">
+              <img
+                className="object-cover p-1"
+                src="/avatar.svg"
+                alt="User Avatar"
+                width={50}
+                height={30}
+              />
+            </span>
           </div>
-        </div>
+        </nav>
       </header>
       <main className="mx-auto max-w-4xl p-4">
         <div>
@@ -40,6 +41,6 @@ export function App() {
 
         <Projects />
       </main>
-    </div>
+    </>
   );
 }
