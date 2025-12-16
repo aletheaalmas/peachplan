73a19fbdf7fe3367e6795844e8c10a7a86cfd6f3
Alethea Almas <alethea.almas@gmail.com>
2025-12-16 14:28:05 +0800

    feat: add project status

--- src/modules/project/type.ts
+++ src/modules/project/type.ts
@@ -1,7 +1,8 @@
+export type ProjectStatus = "To Do" | "In Progress" | "In Review" | "Done";
 export type Project = {
   id: number;
   title: string;
-  isDone: boolean;
+  status: ProjectStatus;
   priority: "P1" | "P2" | "P3" | "P4";
   description: string;
   dueDate: Date;
