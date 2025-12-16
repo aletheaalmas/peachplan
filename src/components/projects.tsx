06dd84d419038ac608586f972b6224bc078a208b
Alethea Almas <alethea.almas@gmail.com>
2025-12-16 15:01:16 +0800

    feat: project card color based on priority

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -6,6 +6,14 @@ import { Trash } from "lucide-react";
 
 const status: ProjectStatus[] = ["To Do", "In Progress", "In Review", "Done"];
 
+const priorityColorMap: Record<Project["priority"], string> = {
+  P0: "bg-red-200",
+  P1: "bg-rose-100",
+  P2: "bg-purple-100",
+  P3: "bg-blue-100",
+  P4: "bg-lime-100",
+};
+
 export function Projects() {
   return (
     <div className="mt-6 grid grid-cols-4 gap-4">
@@ -29,8 +37,9 @@ export function Projects() {
 }
 
 export function ProjectItem({ project }: { project: Project }) {
+  const projectCard = priorityColorMap[project.priority];
   return (
-    <div className="h-40 rounded-2xl bg-red-100 p-4 text-sm">
+    <div className={`h-40 rounded-2xl p-4 text-sm ${projectCard}`}>
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-2">
           <span className="rounded-md bg-orange-100 px-2 py-0.5 text-[10px] font-medium text-red-400">
