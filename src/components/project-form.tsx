79b643ddceb1f89770317918bf001241d076bd7b
Alethea Almas <alethea.almas@gmail.com>
2025-12-17 10:09:48 +0800

    chore: delete label in project form

--- src/components/project-form.tsx
+++ src/components/project-form.tsx
@@ -4,14 +4,8 @@ import { Button } from "./button";
 export function ProjectForm() {
   return (
     <Form.Root className="w-full rounded-2xl border-2 border-gray-200 p-6">
-      {/* <h2 className="mb-4 text-lg font-semibold text-gray-800">Add Project</h2> */}
-
- 
       <Form.Field name="title" className="mb-4">
         <div className="mb-1 flex items-center justify-between">
-          {/* <Form.Label className="text-sm font-medium text-gray-700">
-            Project Name
-          </Form.Label> */}
           <Form.Message match="valueMissing" className="text-xs text-red-500">
             Required
           </Form.Message>
@@ -21,17 +15,13 @@ export function ProjectForm() {
           <input
             required
             placeholder="Project Name"
-            className="w-full rounded-lg px-3 py-2 text-sm outline-none "
+            className="w-full rounded-lg px-3 py-2 text-sm outline-none"
           />
         </Form.Control>
       </Form.Field>
 
-    
       <Form.Field name="description" className="mb-6">
         <div className="mb-1 flex items-center justify-between">
-          {/* <Form.Label className="text-sm font-medium text-gray-700">
-            Description
-          </Form.Label> */}
           <Form.Message match="valueMissing" className="text-xs text-red-500">
             Required
           </Form.Message>
