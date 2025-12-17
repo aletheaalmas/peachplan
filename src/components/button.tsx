4110935c8872d9f042791305fd29a1c9f2a70a54
M Haidar Hanif <git@mhaidarhanif.com>
2025-12-17 13:08:16 +0700

    feat: use shadcn button and color styles

--- src/components/button.tsx
+++
@@ -1,7 +0,0 @@
-export function Button({ children }: { children: React.ReactNode }) {
-  return (
-    <button className="rounded-lg bg-red-400 from-neutral-300 px-4 py-2 text-white transition hover:bg-red-500">
-      {children}
-    </button>
-  );
-}
