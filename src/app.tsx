ec3c72daaf4230d4547f2b5c29c86d810577eaaa
Alethea Almas <alethea.almas@gmail.com>
2025-12-22 13:02:36 +0800

    feat: setup alias shadcn ui

--- src/app.tsx
+++ src/app.tsx
@@ -1,47 +1,7 @@
-import { SearchBar } from "./components/search-bar";
-// import { Button } from "./components/button";
-// import { Welcome } from "./components/welcome";
-import { Projects } from "./components/projects";
-import { ProjectForm } from "./components/project-form";
+import { ComponentExample } from "@/components/component-example";
 
 export function App() {
-  return (
-    <>
-      <header className="w-full border-b border-gray-200 bg-red-200">
-        <nav className="mx-auto flex max-w-4xl items-center justify-between px-3 py-2">
-          <a href="/">
-            <img
-              className="flex gap-3"
-              src="/logo.svg"
-              alt="Peach Plan Logo"
-              width={50}
-              height={30}
-            />
-          </a>
-
-          <SearchBar />
-
-          <div className="flex justify-between gap-4">
-            {/* <Button>+ Add Project</Button> */}
-            <button className="h-10 w-10 overflow-hidden rounded-full bg-gray-100">
-              <img
-                className="object-cover"
-                src="/avatar.svg"
-                alt="User Avatar"
-                width={50}
-                height={50}
-              />
-            </button>
-          </div>
-        </nav>
-      </header>
-      <main className="mx-auto max-w-4xl p-4">
-        {/* <Welcome /> */}
-
-        <ProjectForm />
-
-        <Projects />
-      </main>
-    </>
-  );
+return <ComponentExample />;
 }
+
+export default App;
