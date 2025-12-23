7645304f1bc58e4303ee52e368993ac270a2e9f3
Alethea Almas <alethea.almas@gmail.com>
2025-12-23 10:25:03 +0800

    fix: delete component example

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,45 @@
-import { ComponentExample } from "@/components/component-example";
+import { SearchBar } from "./components/search-bar";
+import { Projects } from "./components/projects";
+import { ProjectForm } from "./components/project-form";
 
 export function App() {
-return <ComponentExample />;
-}
+  return (
+    <>
+      <header className="w-full border-b border-gray-200 bg-red-200">
+        <nav className="mx-auto flex max-w-4xl items-center justify-between px-3 py-2">
+          <a href="/">
+            <img
+              className="flex gap-3"
+              src="/logo.svg"
+              alt="Peach Plan Logo"
+              width={50}
+              height={30}
+            />
+          </a>
 
-export default App;
+          <SearchBar />
+
+          <div className="flex justify-between gap-4">
+            {/* <Button>+ Add Project</Button> */}
+            <button className="h-10 w-10 overflow-hidden rounded-full bg-gray-100">
+              <img
+                className="object-cover"
+                src="/avatar.svg"
+                alt="User Avatar"
+                width={50}
+                height={50}
+              />
+            </button>
+          </div>
+        </nav>
+      </header>
+      <main className="mx-auto max-w-4xl p-4">
+        {/* <Welcome /> */}
+
+        <ProjectForm />
+
+        <Projects />
+      </main>
+    </>
+  );
+}
