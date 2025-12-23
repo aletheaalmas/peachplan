aecbf22576b7f71edb3c00c4aaebee286cb7e33f
Alethea Almas <alethea.almas@gmail.com>
2025-12-23 10:29:15 +0800

    feat: change search bar using shadcn

--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -1,17 +1,17 @@
-import { Search } from "lucide-react";
+import { Search } from "lucide-react"
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from "@/components/ui/input-group"
 
 export function SearchBar() {
   return (
-    <form className="relative w-full max-w-md">
-      <span className="absolute top-1/2 left-3 -translate-y-1/2 text-gray-500">
-        <Search size={18} />
-      </span>
-      <input
-        type="search"
-        name="q"
-        placeholder="Search your projects"
-        className="w-full rounded-lg border-2 border-gray-300 bg-stone-100 py-2 pr-4 pl-10 text-gray-700 placeholder:text-gray-400 focus:ring-2 focus:ring-red-300 focus:outline-none"
-      />
-    </form>
-  );
+    <InputGroup className="max-w-sm bg-stone-100">
+      <InputGroupInput placeholder="Search..." />
+      <InputGroupAddon>
+        <Search className="size-4 text-muted-foreground" />
+      </InputGroupAddon>
+    </InputGroup>
+  )
 }
